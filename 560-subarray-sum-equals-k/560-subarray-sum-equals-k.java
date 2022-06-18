32c9773e7b1ad86e968673d6b7bf3d75c2b80df1
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2022-06-18 19:10:58 +0530

    Time: 36 ms (58.64%), Space: 67.4 MB (25.22%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,8 +1,14 @@
+// Approch
+// 1 - Time - O(n2), Space - O(1)
+// 2-  Time - O(n), Space - O(n+n);
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        
         int n = nums.length;
         int[]  prefixSum = new int[n];
         int sum = 0;
+        
         for(int i = 0; i < n; i++){
             sum += nums[i];
             prefixSum[i] = sum;
@@ -13,19 +19,16 @@ class Solution {
         Map<Integer, Integer> hm = new HashMap<>();
         
         hm.put(0,1);
-        
-        //int currSum = 0;
         int count = 0;
+        
         for(int i = 0; i < n; i++){
             int target = prefixSum[i] - k;
             // if(prefixSum[i] == k) {
             //     //hm.put(prefixSum[i], hm.getOrDefault(prefixSum[i], 0) + 1);
             //     count++;
             // }
-            if(hm.containsKey(target)){
-                //hm.put(prefixSum[i], hm.getOrDefault(prefixSum[i], 0) + 1);
-                count+=hm.get(target);
-            }
+            if(hm.containsKey(target))  count+=hm.get(target);
+
             hm.put(prefixSum[i], hm.getOrDefault(prefixSum[i], 0) + 1);
         } 
         
