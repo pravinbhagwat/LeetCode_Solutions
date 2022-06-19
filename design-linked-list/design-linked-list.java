2b9cb6b915a33bc13c34162ff664462f30d2b2ec
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2022-06-19 13:49:56 +0530

    Time: 14 ms, Memory: 50.5 MB - LeetHub

---
+++ design-linked-list/design-linked-list.java
@@ -0,0 +1,75 @@
+class Node {
+    Node next;
+    int val;
+    public Node() {}
+    public Node(int val) {
+        this.val = val;
+    }
+    public Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class MyLinkedList {
+    //dummy head
+    Node head;
+    int size;
+
+    public MyLinkedList() {
+        this.size = 0;
+        this.head = new Node(-1);
+    }
+    
+    public int get(int index) {
+        if(index >= this.size) return -1;
+        Node curr = this.head;
+        while(index-- >= 0) {
+            curr = curr.next;
+        }
+        return curr.val;
+    }
+    
+    public void addAtHead(int val) {
+        Node tmp = head.next;
+        head.next = new Node(val);
+        head.next.next = tmp;
+        this.size++;
+    }
+    
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index > size) return;
+        Node curr = this.head;
+        while(index-- > 0) {
+            curr = curr.next;
+        }
+        Node tmp = curr.next;
+        curr.next = new Node(val);
+        curr.next.next = tmp;
+        this.size++;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index >= size) return;
+        Node curr = head;
+        while(index-- > 0) {
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        this.size--;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
