ba9ddcea046162a6380dbae898a94f91eafc9e8c
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2022-06-19 22:04:35 +0530

    Time: 1 ms, Memory: 55.4 MB - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,46 +11,16 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
         ListNode tempA = headA, tempB = headB;
-        int count1 = 0;
-        int count2 = 0;
         
-        while(tempA != null){
-            count1++;
-            tempA = tempA.next;
-        }
-        while(tempB != null){
-            count2++;
-            tempB = tempB.next;
-        }
-        
-        tempA = headA;
-        tempB = headB;
-        int skip = 0;
-        if(count1 > count2) {
-            skip = count1 - count2;
+        while(tempA != tempB){
+            if(tempA == null) tempA = headB;
+            else tempA = tempA.next;
+            if(tempB == null) tempB = headA;
+            else tempB = tempB.next;
             
-            while(skip-- > 0){
-                tempA = tempA.next;
-            }
-            while(tempA != null && tempB != null){
-                if(tempA == tempB) return tempA;
-                tempA = tempA.next;
-                tempB = tempB.next;
-            }            
         }
-        else{
-            skip = count2 - count1;
-            while(skip-- > 0){
-                tempB = tempB.next;
-            }
-            while(tempA != null && tempB != null){
-                if(tempA == tempB) return tempA;
-                tempA = tempA.next;
-                tempB = tempB.next;
-            }            
-        }        
-        
-        return null;
+        return tempA;
     }
 }
