851b849254be9428f505306e9e5587ee5705efe6
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2022-07-11 19:09:14 +0530

    Added solution - LeetHub

---
+++ Next Greater Element - GFG/next-greater-element.java
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    long[] res = new Solution().nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        System.out.print(res[i] + " ");
+		    System.out.println();
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        // Your code here
+        long[] output = new long[n];
+        Arrays.fill(output, -1);
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i = 0; i < n; i++){
+            while(!st.isEmpty() && arr[i] > arr[st.peek()]){
+            output[st.peek()] = arr[i];
+            st.pop();
+            }
+            st.push(i);
+        }
+        
+        return output;
+    } 
+    
+}
