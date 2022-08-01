2eda13b2ef3d75582ab5a8422ba2b422d94d78dd
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2022-08-01 22:00:59 +0530

    Time: 0 ms (100.00%), Space: 50.9 MB (68.15%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int missingNumber(int[] nums) {
+    // Approach 1 - Frequecy Array
+    private int solve(int[] nums){
         int n = nums.length;
         int[] count = new int[n+1];
         
@@ -12,5 +13,22 @@ class Solution {
         }
         
         return -1;
+    }
+    
+    // Approach 2
+    private int solve2(int[] nums){
+        int n = nums.length;
+        int ans = n*(n+1);
+        ans /= 2;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+        }
+        
+        return ans-sum;
+    }
+    
+    public int missingNumber(int[] nums) {
+        return solve2(nums);
     }
 }
