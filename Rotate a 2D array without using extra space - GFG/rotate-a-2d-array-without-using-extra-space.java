6c97eb0a0c43ea6428de9fa5072a822a027a405b
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2022-10-10 20:22:55 +0530

    Added solution - LeetHub

---
+++ Rotate a 2D array without using extra space - GFG/rotate-a-2d-array-without-using-extra-space.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * n + j]);
+                }
+            }
+
+            new Solution().rotateMatrix(arr, n);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    out.print(arr[i][j] + " ");
+                }
+            }
+            out.println();
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    void rotateMatrix(int arr[][], int n) {
+        // code here
+        
+        for(int i = 0; i < n; i++) {
+            int start = 0;
+            int end = arr[i].length-1;
+            while(start <= end) {
+                int temp = arr[i][start];
+                arr[i][start] = arr[i][end];
+                arr[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < arr[0].length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        
+    }
+}
